dee5feffbb3dbca1116f910f3fa110bdac1f2339
raksupermarket <55552565+raksupermarket@users.noreply.github.com>
2020-08-18 19:26:07 +0700

    Create slider.js

---
+++ slider.js
@@ -0,0 +1,4 @@
+var app = new Vue({ 
+    el: '#app',
+});
+document.write( '<center><p><small>A blog design by <a href=\'https://axcoradesign.blogspot.com/\' rel=\'dofollow\' target=\'blank\'>Axcora Design</a> Digital ART <a href=\'https://mesinkasironline.web.app\' rel=\'dofollow\' target=\'blank\'>mesinkasironline</a></small></p></center>' );
